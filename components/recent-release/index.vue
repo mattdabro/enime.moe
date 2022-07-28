2923d3c4a1b3abfecaddd843c410b30d239f80c8
Aqil Contractor <aqilcm@gmail.com>
2022-07-28 02:08:41 -0400

    FINALLY GOT APPENDING WORKING WOO

--- components/recent-release/index.vue
+++ components/recent-release/index.vue
@@ -7,7 +7,7 @@
     <div @scroll="scroll" id="eps" ref="eps"
       class="grid grid-rows-2 col-auto row-auto grid-flow-col-dense m-0 overflow-x-scroll w-screen pr-5">
       <nuxt-link :nuxt-child-key="episode.id" :to="`/watch/${episode.id}`" :key="episode.id"
-        v-for="(episode, index) in recent.data" class="p-0 m-0">
+        v-for="(episode, index) in recent" class="p-0 m-0">
         <episode-card :key="episode.id" :anime="episode.anime" :title="episode.title" :id="episode.id"
           :number="episode.number" :createdAt="episode.createdAt" />
       </nuxt-link>
@@ -30,13 +30,25 @@
   const rows = Math.ceil(window.innerWidth / convertRemToPixels(11)) * 2;
 
   const page = ref(1);
+  const recent = ref([]);
 
-  let { data: recent, refresh } = await useFetch(() => `https://api.enime.moe/recent?page=${page.value++}&perPage=${rows}`);
+
+  const { data } = await useFetch(() => `https://api.enime.moe/recent?page=${page.value}&perPage=${rows}`)
+  recent.value = data.value.data as any[];
+
+watch(page, async () => {
+    console.log(page.value);
+    console.log(recent.value);
+    const { data } = await useFetch(() => `https://api.enime.moe/recent?page=${page.value}&perPage=${rows}`);
+    for (let i = 0; i < rows; i++) {
+      recent.value.push(data.value.data[i]);
+    }
+  });
 
   async function scroll(e: Event) {
     const eps = e.target as HTMLElement;
     if (eps.scrollLeft + eps.clientWidth >= eps.scrollWidth) {
-      refresh();
+      page.value++;
       eps.scrollLeft = eps.scrollWidth;
     }
   }
