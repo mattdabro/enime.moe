98e409816085a97cd418f5cbb1db2d6823cb7d14
NADESHIKON <allenzhang040817@gmail.com>
2022-07-28 20:52:21 -0700

    Use Google Tag Manager

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -56,12 +56,15 @@ export default defineNuxtConfig({
     app: {
         head: {
             script: [
-                { children: `window.dataLayer = window.dataLayer || [];\n` +
-                        "  function gtag(){dataLayer.push(arguments);}\n" +
-                        "  gtag('js', new Date());\n" +
-                        "\n" +
-                        `  gtag('config', "G-57VGSBQP6C");` },
-                { src: `https://www.googletagmanager.com/gtag/js?id=G-57VGSBQP6C`, async: true/*, type: 'text/partytown'*/ },
+                {
+                    hid: "gtm",
+                    children: `(function(w,d,s,l,i){w[l]=w[l]||[];w[l].push({'gtm.start':
+      new Date().getTime(),event:'gtm.js'});var f=d.getElementsByTagName(s)[0],
+      j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';j.async=true;j.src=
+      'https://www.googletagmanager.com/gtm.js?id='+i+dl;f.parentNode.insertBefore(j,f);
+      })(window,document,'script','dataLayer','GTM-TF7R748');`,
+                    type: "text/javascript"
+                }
             ],
         },
     },
