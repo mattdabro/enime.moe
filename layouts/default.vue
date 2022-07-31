425fc9b6b4a09ce1b1ca257072c0191d4562f05b
Aqil Contractor <aqilcm@gmail.com>
2022-07-31 16:38:16 -0400

    Build framework for search, will upload search page later.

--- layouts/default.vue
+++ layouts/default.vue
@@ -1,16 +1,20 @@
 <template>
   <div class="bg-black text-white h-screen w-screen flex flex-col">
-    <div class="flex justify-items-center items-center h-50 mx-auto mt-5 gap-8">
-      <nuxt-link to="/" class="text-4xl text-tertiary">Explore</nuxt-link>
-      <div class="text-4xl text-tertiary flex flex-row flex-nowrap items-center">
-        <svg width="1.5rem" fill="none" viewBox="0 0 24 24" stroke="currentColor"
-          stroke-width="4">
-          <path stroke-linecap="round" stroke-linejoin="round" d="M21 21l-6-6m2-5a7 7 0 11-14 0 7 7 0 0114 0z" />
-        </svg>
-        <span>Search</span>
+    <div class="nav-cont">
+      <div class="inline-flex items-center gap-8 h-full mx-auto relative">
+        <nuxt-link to="/" class="text-4xl text-tertiary">Explore</nuxt-link>
+        <div class="grid">
+          <div class="urmom text-4xl text-tertiary flex flex-row flex-nowrap items-center px-2">
+            <svg width="1.5rem" fill="none" viewBox="0 0 24 24" stroke="currentColor" stroke-width="4">
+              <path stroke-linecap="round" stroke-linejoin="round" d="M21 21l-6-6m2-5a7 7 0 11-14 0 7 7 0 0114 0z" />
+            </svg>
+            <span class="pl-1">Search</span>
+          </div>
+          <input :submit="search" placeholder=" " class="urmom in p-2 text-4xl w-40" />
+        </div>
       </div>
     </div>
-    <div class="flex-grow w-screen p-0 m-0">
+    <div class="flex-grow w-screen p-0 m-0 mt-20">
       <slot />
     </div>
     <footer class="bg-[#222] p-10">
@@ -40,13 +44,46 @@
   </div>
 </template>
 
-<script>
+<script lang="ts">
+
 export default {
-  name: "default"
+  name: "default",
+  methods: {
+    search() {
+
+    }
+  }
 }
 </script>
 
-<style scoped>
+<style scoped lang="scss">
+  .nav-cont {
+    position: fixed;
+    top: 0px;
+    left: 0px;
+    right: 0px;
+    z-index: 100;
+    height: 5rem;
+    width: 100%;
+    overflow: hidden;
+    background-color: black;
+    display: flex;
+    
+    flex-direction: column;
+  }
+  .urmom {
+    grid-column: 1;
+    grid-row: 1;
+  }
+  .in {
+    background-color: transparent; /* #fff3; */
+    border-radius: .6rem;
+
+    &:not(:placeholder-shown) {
+      background-color: black;
+      border: 3px solid #333;
+    }
+  }
   .disclaimer {
     flex: 3;
   }
